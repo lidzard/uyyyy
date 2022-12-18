6124ebae5b73e0c7190cff33437d05f99f9dca37
lidzard <90536035+lidzard@users.noreply.github.com>
2022-12-17 19:24:15 -0700

    Add files via upload

---
+++ cup.js
@@ -0,0 +1,27 @@
+Status = "";
+img = "";
+function preload() {
+    img = loadImage("cup.jpg")
+}
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting...";
+}
+function draw() {
+    image(img, 0, 0, 500, 500);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    objectdetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
+}
